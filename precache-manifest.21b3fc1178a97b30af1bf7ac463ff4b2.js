5a01e35795098bb68cdb9c1f1796605e38370613
Oleg Lobach <oleg@lobach.info>
2020-08-11 21:02:20 +0300

    Initial release

---
+++ precache-manifest.21b3fc1178a97b30af1bf7ac463ff4b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e74cc812e826319046dcd1164de7374",
+    "url": "./index.html"
+  },
+  {
+    "revision": "040c968e32e58fd9f38c",
+    "url": "./static/css/main.dc6f3e3f.chunk.css"
+  },
+  {
+    "revision": "cc486035cec41f57e1bc",
+    "url": "./static/js/2.6374cf77.chunk.js"
+  },
+  {
+    "revision": "b80a9f81801735e6ab25697277ea8ec5",
+    "url": "./static/js/2.6374cf77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040c968e32e58fd9f38c",
+    "url": "./static/js/main.bd6693c8.chunk.js"
+  },
+  {
+    "revision": "a55b6a37224ba95399de",
+    "url": "./static/js/runtime-main.f626f5fa.js"
+  },
+  {
+    "revision": "6d6e8ad12d26f5ff08249f5bb241dc80",
+    "url": "./static/media/roboto-v19-latin_cyrillic-ext-regular.6d6e8ad1.ttf"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "./static/media/roboto-v19-latin_cyrillic-ext-regular.8681f434.svg"
+  },
+  {
+    "revision": "c3bde69bd8b9ebfa0e8b3b9f71c9dac4",
+    "url": "./static/media/roboto-v19-latin_cyrillic-ext-regular.c3bde69b.eot"
+  },
+  {
+    "revision": "f5e9a4eaaa88c13e9ebdb1751ef43789",
+    "url": "./static/media/roboto-v19-latin_cyrillic-ext-regular.f5e9a4ea.woff"
+  },
+  {
+    "revision": "fa18727aae742d5ccd18c6ab4470043e",
+    "url": "./static/media/roboto-v19-latin_cyrillic-ext-regular.fa18727a.woff2"
+  }
+]);
